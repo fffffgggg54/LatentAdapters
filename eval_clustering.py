917259e0ba13a0a8975b492462698b9adc06734a
Fredo Guan <fredo.guan@hotmail.com>
2025-12-21 12:50:05 -0800

    Update eval_clustering.py

--- eval_clustering.py
+++ eval_clustering.py
@@ -7,7 +7,7 @@ import torch
 
 
 out_dir = "outputs/scratch_mmID_8192_discriminator1.0_latent1.0_MSE_JointTraining/"
-file_path = out_dir + "Pairwise Pearson Correlation Coefficient of Latents (mscoco).png"
+file_path = out_dir + "Pairwise Pearson Correlation Coefficient of Adapted Embeds (mscoco).png"
 
 def read_csv_from_image(image_path):
     """
