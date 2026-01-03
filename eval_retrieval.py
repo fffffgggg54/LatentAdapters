bae152e5c20acae8591063dfef0821a85cea18c4
Fredo Guan <fredo.guan@hotmail.com>
2026-01-02 21:11:33 -0800

    Update eval_retrieval.py

--- eval_retrieval.py
+++ eval_retrieval.py
@@ -259,7 +259,7 @@ def main():
         # [N, N, B, D]
         # out_model, in_model, batch_idx, dim
         all_adapted_embeds = adapter.fw_latent_to_all_embeds(all_latents)
-        all_adapted_embeds = all_adapted_embeds.transpose(0, 1)
+        all_adapted_embeds = all_adapted_embeds
         all_adapted_embeds_normalized = F.normalize(all_adapted_embeds, p=2, dim=-1)
         #all_adapted_embeds_normalized = all_adapted_embeds_normalized.to('cpu', non_blocking=True)
         
