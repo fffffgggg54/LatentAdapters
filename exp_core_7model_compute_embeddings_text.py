b8b6b76629e7b887a80b6ccc8bce1aebe237103d
Fredo Guan <fredo.guan@hotmail.com>
2026-01-10 13:25:33 -0800

    experiment with more text models

--- exp_core_7model_compute_embeddings_text.py
+++ exp_core_7model_compute_embeddings_text.py
@@ -47,7 +47,10 @@ class ImageOnlyDataset(torch.utils.data.Dataset):
         return image, 0
 
 #ds = datasets.load_dataset('laion/conceptual-captions-12m-webdataset')['train']
+#label = "cc12m_text"
 ds = datasets.load_dataset('clip-benchmark/wds_mscoco_captions2017')['test']
+label = "mscoco_captions2017_test_text"
+
 ds_train = ImageOnlyDataset(ds, column = 'txt')
 
 def last_token_pool(last_hidden_states, attention_mask):
@@ -59,6 +62,9 @@ def last_token_pool(last_hidden_states, attention_mask):
         batch_size = last_hidden_states.shape[0]
         return last_hidden_states[torch.arange(batch_size, device=last_hidden_states.device), sequence_lengths]
 
+def average_pool(last_hidden_states, attention_mask):
+    last_hidden = last_hidden_states.masked_fill(~attention_mask[..., None].bool(), 0.0)
+    return last_hidden.sum(dim=1) / attention_mask.sum(dim=1)[..., None]
 
 def get_detailed_instruct(task_description: str, query: str) -> str:
     return f'Instruct: {task_description}\nQuery:{query}'
@@ -74,7 +80,6 @@ def compute_embeddings_qwen():
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     model = AutoModel.from_pretrained(model_name, attn_implementation="sdpa").to(device)
 
-    label = "mscoco_captions2017_test_text"
     model_tag = "qwen3_embedding_4b_bf16"
     
     num_workers = 6
@@ -137,9 +142,89 @@ def compute_embeddings_openclip():
     del embeds
     gc.collect()
 
+def compute_embeddings_e5():
+    # Base batch size per GPU
+    base_bs = 128
+    model_name = "intfloat/e5-large-v2"
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModel.from_pretrained(model_name, attn_implementation="sdpa").to(device)
 
+    model_tag = "e5_large_v2"
+    
+    num_workers = 30
+    loader = torch.utils.data.DataLoader(ds_train, batch_size=base_bs, num_workers=num_workers)
+    
+    all_batches = []
+    
+    for textBatch, _ in tqdm.tqdm(loader, desc=f"Computing embeddings for {label}"):
+        with torch.autograd.grad_mode.inference_mode(), torch.amp.autocast(device_type="cuda", dtype=autocast_dtype):
+            batch_dict = tokenizer(
+                textBatch,
+                max_length=512,
+                padding=True,
+                truncation=True,
+                return_tensors="pt",
+            )
+            batch_dict.to(model.device)
+            outputs = model(**batch_dict)
+            embedsBatch = average_pool(outputs.last_hidden_state, batch_dict['attention_mask']).to('cpu', non_blocking=True).bfloat16()
+            all_batches.append(embedsBatch)
 
-compute_embeddings_openclip() 
-compute_embeddings_qwen()
- 
+    embeds = torch.cat(all_batches, dim=0)
+
+    save_path = f'/scratch/fyguan/embeds_{label}_{model_tag}.pt'
+    print(f"Saving embeddings to {save_path}")
+    torch.save(embeds, save_path)
+
+    del all_batches
+    del embeds
+    gc.collect()
+
+# any model that takes the first embedding of the last layer as the output
+def compute_embeddings_bert(model_name):
+    # Base batch size per GPU
+    base_bs = 128
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModel.from_pretrained(model_name, attn_implementation="sdpa").to(device)
+
+    model_tag = model_name.split('/')[1].replace('-', '_')
+    
+    num_workers = 30
+    loader = torch.utils.data.DataLoader(ds_train, batch_size=base_bs, num_workers=num_workers)
+    
+    all_batches = []
+    
+    for textBatch, _ in tqdm.tqdm(loader, desc=f"Computing {model_name} embeddings for {label}"):
+        with torch.autograd.grad_mode.inference_mode(), torch.amp.autocast(device_type="cuda", dtype=autocast_dtype):
+            batch_dict = tokenizer(
+                textBatch,
+                max_length=model.config.max_position_embeddings,
+                padding=True,
+                truncation=True,
+                return_tensors="pt",
+            )
+            batch_dict.to(model.device)
+            outputs = model(**batch_dict)
+            embedsBatch = outputs.last_hidden_state[:, 0].to('cpu', non_blocking=True).bfloat16()
+            all_batches.append(embedsBatch)
+
+    embeds = torch.cat(all_batches, dim=0)
+
+    save_path = f'/scratch/fyguan/embeds_{label}_{model_tag}.pt'
+    print(f"Saving embeddings to {save_path}")
+    torch.save(embeds, save_path)
+
+    del all_batches
+    del embeds
+    gc.collect()
+
+#compute_embeddings_openclip() 
+#compute_embeddings_qwen()
+compute_embeddings_e5()
+compute_embeddings_bert('google-bert/bert-large-uncased')
+compute_embeddings_bert('FacebookAI/roberta-large')
+compute_embeddings_bert('Alibaba-NLP/gte-en-mlm-large')
+compute_embeddings_bert('Alibaba-NLP/gte-large-en-v1.5')
+compute_embeddings_bert('Alibaba-NLP/gte-modernbert-base')
+compute_embeddings_bert('Alibaba-NLP/gte-multilingual-base')
     
